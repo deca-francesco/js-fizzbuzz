5f713b2b710bf2c259a8d3bdccf04b0ce55709f7
deca-francesco <francescodecarlo70@gmail.com>
2024-10-01 16:06:33 +0200

    UPDATE: comments

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log("js linked");
 
 
 /*
+Consegna:
 Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
@@ -15,7 +16,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 - condizionali if/else if/else
 */
 
-// Devo fare un ciclo for con all'interno le if per i multipli di 3, di 5 e di entrambi.
+// Devo fare un ciclo for con all'interno le if per i multipli di 3, di 5, di entrambi e di nessuno dei 2.
 // Ho trovato che posso usare l'operatore % per verificare quando un numero può essere esattamente divisibile per un altro
 // inserisco i diversi log nelle diverse if
 
