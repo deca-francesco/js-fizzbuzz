3cc3dcaa956a3051365b0d8aecea4b7e8a312bc1
deca-francesco <francescodecarlo70@gmail.com>
2024-10-01 15:40:33 +0200

    intial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log("js linked");
+
+
+/*
+Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+*/
+
+// 0) Strumenti conosciuti
+/*
+- ciclo for
+- console.log
+- operatori aritmetici, di comparazione e logici
+- numeri
+- condizionali if/else if/else
+*/
+
+// 1) Dichiarazione variabili
+
+// 2) Raccolta dati
+
+// 3) Elaborazione dati
+
+// 4) Output
